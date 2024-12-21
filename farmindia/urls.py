89d7c35c4063832655396d6d2d67d2dea77bdb08
Dinesh <pidugudinesh.22.it@anits.edu.in>
2024-12-21 15:22:07 +0530

    added

--- farmindia/urls.py
+++ farmindia/urls.py
@@ -18,7 +18,9 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.urls import include,path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',include('web.urls'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- web/views.py
+++ web/views.py
@@ -1,3 +1,36 @@
 from django.shortcuts import render
+# import os
+# import google.generativeai as genai
+# from dotenv import load_dotenv
 
-# Create your views here.
+# load_dotenv()
+# api_key = os.getenv("GEMINI_API_KEY")
+# if api_key is None:
+#     print("API Key not found! Check the .env file.")
+# genai.configure(api_key=api_key)
+
+# generation_config = {
+#   "temperature": 1,
+#   "top_p": 0.95,
+#   "top_k": 40,
+#   "max_output_tokens": 8192,
+#   "response_mime_type": "text/plain",
+# }
+
+# model = genai.GenerativeModel(
+#   model_name="gemini-2.0-flash-exp",
+#   generation_config=generation_config,
+# )
+
+# chat_session = model.start_chat(
+#   history=[
+#   ]
+# )
+# x=str(input())
+# response = chat_session.send_message(x)
+
+# print(response.text)
+def login(request):
+    return render(request,'login.html')
+def home(request):
+    return render(request,'home.html')
